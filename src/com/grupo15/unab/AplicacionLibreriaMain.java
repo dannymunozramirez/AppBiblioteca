5c54915d091016bcb2435a0ebfc31ac06a93a20b
dannymunozramirez <danny.munoz.ramirez@gmail.com>
2022-11-15 20:06:38 -0500

    Added some logic within Clientes and Validaciones

--- src/com/grupo15/unab/AplicacionLibreriaMain.java
+++ src/com/grupo15/unab/AplicacionLibreriaMain.java
@@ -15,7 +15,7 @@ public class AplicacionLibreriaMain {
 
     public static void main(String[] args) {
 
-        // Lista de usuarios para pruebas
+        // Lista de usuarios para pruebas - Esta lista debe venir de un archivo
         List<Usuario> usuarios = new ArrayList<>(Arrays.asList(
                 new Docente("1-3","Test","M","magister"),
                 new Docente("1-9","Test","M","magister"),
@@ -35,7 +35,22 @@ public class AplicacionLibreriaMain {
         // Imprime la lista con un cliente nuevo en caso que no exista en la primera lista
         clientes.getUsuarios().forEach(usuario -> System.out.println(usuario.getRun()));
 
+
+
+
     }
+    static final String NOMBRE ="un valor";
+
+//    public static ArrayList<Usuario>
+
+    // File archivo
+
+    // Se puede usar un JSON en vez de CVS ---> TRUE
+
+
+    // TODO
+    // Leer JSON - Escribir JSON
+
 
 
 }
